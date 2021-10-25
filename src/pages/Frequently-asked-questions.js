a1af777180819e9c15d70e877dd710b45cb4ae13
“Elena” <elenapavlic@yahoo.com>
2021-10-25 21:46:28 +0300

    Created the form component

--- src/pages/Frequently-asked-questions.js
+++ src/pages/Frequently-asked-questions.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout/layout";
 import FaqQuestions from "../components/faq/faq-questions/faq-questions";
+import ContactForm from "../components/form/contact-form";
 import { Container } from "bloomer";
 
 export default function FaqPage() {
@@ -10,6 +11,9 @@ export default function FaqPage() {
                 <Container>
                     <div className="wrapper">
                         <FaqQuestions />
+                    </div>
+                    <div className="wrapper">
+                        <ContactForm />
                     </div>
                 </Container>
             </section>
