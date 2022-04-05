5b3045d66655d46f3c3d132e3766f9c32014cf71
Bogdan Dogaru <bogdan5dogaru@gmail.com>
2022-04-05 18:15:04 +0100

    Added Splide - not finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
-import React from "react";
-import Layout from "../components/layout/layout";
-import HomepageHero from "../components/homepage-hero/homepage-hero";
-import HomepageServices from "../components/homepage-services/homepage-services";
-import { Helmet } from "react-helmet";
-import ReviewsSection from "../components/reviews-section/reviews-section";
+import React from 'react';
+import Layout from '../components/layout/layout';
+import HomepageHero from '../components/homepage-hero/homepage-hero';
+import HomepageServices from '../components/homepage-services/homepage-services';
+import { Helmet } from 'react-helmet';
+import ReviewsSection from '../components/reviews-section/reviews-section';
+import ProjectsList from '../components/projects-list/projects-list';
 
 export default function Homepage() {
-    return (
-        <Layout>
-            <HomepageHero />
-            <HomepageServices />
-            <ReviewsSection />
-            <Helmet>
-                <script
-                    type="text/javascript"
-                    src="https://widget.clutch.co/static/js/widget.js"
-                ></script>
-            </Helmet>
-        </Layout>
-    );
+	return (
+		<Layout>
+			<HomepageHero />
+			<HomepageServices />
+			<ReviewsSection />
+			<ProjectsList />
+			<Helmet>
+				<script type="text/javascript" src="https://widget.clutch.co/static/js/widget.js"></script>
+			</Helmet>
+		</Layout>
+	);
 }
