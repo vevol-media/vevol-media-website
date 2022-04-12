b95ee96034c2782edcfd4f6fe100419c38d79217
lp <laur@pop-os.localdomain>
2022-04-12 10:21:18 +0300

    team info

--- src/components/team-section/team-info.js
+++ src/components/team-section/team-info.js
@@ -5,12 +5,9 @@ import {Title, Content } from "bloomer";
 const TeamInfo = () => {
     return (
         <Content className="info-container">
-            <div className="info-tilte-section">
-                <span></span>
-                <div className="info-titles-container">
-                    <h1 className="info-title">Company</h1>
-                    <Title  className="info-subtitle" isSize={1}>Professional work team in their field</Title>
-                </div>
+            <div className="info-tilte-section">  
+                <h1 className="info-title">Company</h1>
+                <Title  className="info-subtitle" isSize={1}>Professional work team in their field</Title>
             </div>          
             <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
         </Content>
