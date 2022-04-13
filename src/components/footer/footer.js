cec8200d8497b5a644a5b0b00d71ac63a75b1964
lp <laur@pop-os.localdomain>
2022-04-13 17:06:51 +0300

    bloomer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
-import { Container, Title } from 'bloomer';
+import { Column, Title, Columns } from 'bloomer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import logo from "../../images/logo-vevol-media.svg";
+// import logo from "../../images/logo-vevol-media.svg";
 import './footer.scss';
 
 export default function Footer() {
@@ -12,74 +12,79 @@ export default function Footer() {
 
 	return (
 		<footer>
-			<div className="footer-content">
+			<Columns isCentered>
+				<Column isSize={{ desktop: 2 }}>
+					<div className="footer-logo">
+						<StaticImage
+							src="../../images/logo-vevol-media.svg"
+							alt="avatar"
+							layout="fullWidth"
+						/>
+					</div>
+				</Column>
 
-				<Link to="/" className="footer-logo">
-                    <img src={logo} alt="Vevol Media Logo" />
-                </Link>
-
-
-				<div className="footer-column">
+				<Column isSize={{desktop: 3, widescreen: 2}} className="is-flex is-flex-direction-column">
 					<Title tag="h4" isSize={4}>
 						Services
 					</Title>
 					<Link to="/">Digital Marketing</Link>
 					<Link to="/">Website Development</Link>
-					<Link to="/">Branding & Creative</Link>
-					<Link to="/">Email & SMS Marketing</Link>
-				</div>
+					<Link to="/">Branding &amp; Creative</Link>
+					<Link to="/">Email &amp; SMS Marketing</Link>
+				</Column>
 
-				<div className="footer-column">
+				<Column isSize={{ desktop: 2 }} className="is-flex is-flex-direction-column">
 					<Title tag="h4" isSize={4}>
 						Company
 					</Title>
 					<Link to="/">Story</Link>
 					<Link to="/">Work</Link>
 					<Link to="/">Contact Us</Link>
-				</div>
+				</Column>
 
-				<div className="footer-column">
+				<Column isSize={{ desktop: 2 }} className="is-flex is-flex-direction-column">
 					<Title tag="h4" isSize={4}>
 						Extras
 					</Title>
 					<Link to="/">Client Portal</Link>
 					<Link to="/">Careers</Link>
 					<Link to="/">Limodify</Link>
-					<Link to="/">FAQ</Link>
-				</div>
+					<Link to="/frequently-asked-questions">FAQ</Link>
+				</Column>
 
-				<div className="footer-column">
+				<Column isSize={{ desktop: 2 }}>
 					<Title tag="h4" isSize={4}>
 						Social
 					</Title>
-					<div className="footer-row">
-						<a
-							href="https://m.facebook.com/vevolmediaweb"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<FontAwesomeIcon icon={faFacebookF} />
-						</a>
-						<a
-							href="https://www.instagram.com/vevolmedia"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<FontAwesomeIcon icon={faInstagram} />
-						</a>
-						<a
-							href="https://mobile.twitter.com/vevolmedia"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<FontAwesomeIcon icon={faTwitter} />
-						</a>
-						<a href="#" target="_blank" rel="noopener noreferrer">
-							<FontAwesomeIcon icon={faYoutube} />
-						</a>
-					</div>
-				</div>
-			</div>
+					<a
+						className="mr-5"
+						href="https://m.facebook.com/vevolmediaweb"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon icon={faFacebookF} />
+					</a>
+					<a
+						className="mr-5"
+						href="https://www.instagram.com/vevolmedia"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon icon={faInstagram} />
+					</a>
+					<a
+						className="mr-5"
+						href="https://mobile.twitter.com/vevolmedia"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon icon={faTwitter} />
+					</a>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						<FontAwesomeIcon icon={faYoutube} />
+					</a>
+				</Column>
+			</Columns>
 
 			<div className="footer-copyright">
 				<Link to="/"> © {year} Vevol Media | All Rights Reserved </Link>
