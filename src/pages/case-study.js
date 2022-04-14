9c241ea5a59eeda679ac570152dfd6e08090d771
lp <laur@pop-os.localdomain>
2022-04-14 14:17:52 +0300

    init

---
+++ src/pages/case-study.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Layout from '../components/layout/layout';
+import '../styles/case-study.scss';
+
+const CaseStudy = () => {
+	return (
+		<Layout>
+			<h1 >
+                CaseStudy
+			</h1>
+		</Layout>
+	);
+};
+export default CaseStudy;
