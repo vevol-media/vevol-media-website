5a17ef0d9d2b7637664ff462ba4fd46a7308c8cd
lp <laur@pop-os.localdomain>
2022-04-15 11:29:33 +0300

    info page intro structure

--- src/components/case-study/info-page-intro.js
+++ src/components/case-study/info-page-intro.js
@@ -1,11 +1,35 @@
 import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Content } from 'bloomer';
 import './info-page-intro.scss';
 
 const InfoPageIntro = () => {
 	return (
-		<h1>
-            InfoPageIntro
-		</h1>
+		<div>
+           <StaticImage
+			// src=""
+			alt="avatar"
+			layout="fullWidth"
+		   />
+
+		   <Content>
+			   <div>
+				<div>
+					<h4>Services</h4>
+					<p>Text1</p>
+				</div>
+				<div>
+					<h4>Industry</h4>
+					<p>Text2</p>
+				</div>
+			   </div>
+
+			   <div>
+					<h4>Overview</h4>
+					<p>Text3</p>
+				</div>
+		   </Content>
+		</div>
 	);
 };
 export default InfoPageIntro;
--- src/pages/case-study.js
+++ src/pages/case-study.js
@@ -4,15 +4,24 @@ import InfoPageIntro from '../components/case-study/info-page-intro';
 import InfoPageText from '../components/case-study/info-page-text';
 import SimpleImageCarousel from '../components/case-study/simple-image-carousel';
 import InformationalContent from '../components/case-study/informational-content';
+import SlimHero from '../components/slim-hero/slim-hero';
+import { Container } from 'bloomer';
 import '../styles/case-study.scss';
 
 const CaseStudy = () => {
 	return (
 		<Layout>
-			<InfoPageIntro/>
-            <InfoPageText/>
-            <SimpleImageCarousel/>
-            <InformationalContent/>
+			<SlimHero
+				heading="MARGEE SHOPIFY STORE"
+				subheading="Branding, Email &amp; SMS Marketing, Website Development"
+			/>
+
+			<Container>
+				<InfoPageIntro/>
+				<InfoPageText/>
+				<SimpleImageCarousel/>
+				<InformationalContent/>
+			</Container>
 		</Layout>
 	);
 };
