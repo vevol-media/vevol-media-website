6f90d94c65dcc28804f1f199791de9341cacd0cc
lp <laur@pop-os.localdomain>
2022-04-18 20:46:59 +0300

    added url stores

--- src/components/projects-list/projects-list.js
+++ src/components/projects-list/projects-list.js
@@ -4,12 +4,42 @@ import './projects-list.scss';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import { getImage } from 'gatsby-plugin-image';
-import { convertToBgImage, BgImage } from "gbimage-bridge"
-// import BackgroundImage from 'gatsby-background-image'
+import { convertToBgImage, BgImage } from 'gbimage-bridge';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faExternalLinkSquare, faExternalLinkAlt } from '@fortawesome/free-brands-svg-icons';
+import storesList from '../../enums/stores';
 import '@splidejs/splide/dist/css/splide.min.css';
 
 export default function ProjectsList({ data }) {
 	const nodes = data.allFile.nodes;
+	const splides = nodes.map((photo, index) => {
+		const bgImage = getImage(photo.childImageSharp.gatsbyImageData);
+
+		return storesList.map((store, index) => {
+			if (store.name === photo.name) {
+				return (
+					<SplideSlide key={index}>
+						<BgImage className="bg-image" image={bgImage}>
+							<div className="ml-5 mb-5">
+								<p className='pb-3'>{photo.name}</p>
+								<a
+									href={store.url}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									View Case Study
+									{/* <FontAwesomeIcon icon={faExternalLinkAlt} /> */}
+									<span className='ml-1'>
+										<svg xmlns="http://www.w3.org/2000/svg" width={10} height={10}  viewBox="0 0 512 512"><path d="M384 320c-17.67 0-32 14.33-32 32v96H64V160h96c17.67 0 32-14.32 32-32s-14.33-32-32-32L64 96c-35.35 0-64 28.65-64 64V448c0 35.34 28.65 64 64 64h288c35.35 0 64-28.66 64-64v-96C416 334.3 401.7 320 384 320zM488 0H352c-12.94 0-24.62 7.797-29.56 19.75c-4.969 11.97-2.219 25.72 6.938 34.88L370.8 96L169.4 297.4c-12.5 12.5-12.5 32.75 0 45.25C175.6 348.9 183.8 352 192 352s16.38-3.125 22.62-9.375L416 141.3l41.38 41.38c9.156 9.141 22.88 11.84 34.88 6.938C504.2 184.6 512 172.9 512 160V24C512 10.74 501.3 0 488 0z"/></svg>
+									</span>
+								</a>
+							</div>
+						</BgImage>
+					</SplideSlide>
+				);
+			}
+		});
+	});
 
 	return (
 		<div className="projects-list vm-section">
@@ -24,10 +54,10 @@ export default function ProjectsList({ data }) {
 				<Splide
 					className="projects__list"
 					options={{
-                        rewind: true,
+						rewind: true,
 						perPage: 4,
 						gap: '1rem',
-						height:400,
+						height: 400,
 
 						breakpoints: {
 							1140: {
@@ -46,32 +76,7 @@ export default function ProjectsList({ data }) {
 						},
 					}}
 				>
-					{
-						nodes.map((photo, index) => {
-							const bgImage = getImage(photo.childImageSharp.gatsbyImageData);
-							// const bgImageConv = convertToBgImage(bgImage);
-							return (
-								<SplideSlide key={ index }>
-									{/* <BackgroundImage
-										Tag="section"
-										// className={className}
-										// fluid={bgImageConv}
-										{...bgImage}
-										preserveStackingContext
-										>
-										<p>{photo.name}</p>
-									</BackgroundImage> */}
-
-									<BgImage
-										className='bg-image'
-										image={bgImage}
-									>
-										<p>{photo.name}</p>
-									</BgImage>
-							</SplideSlide>
-							)
-						})
-					}
+					{splides}
 				</Splide>
 			</Container>
 		</div>
---
+++ src/enums/stores.js
@@ -0,0 +1,32 @@
+const storesList = [
+	{
+        name: 'A&S Market',
+		url: 'https://asmarket.co.uk',
+	},
+	{
+        name: 'Evcoms',
+		url: 'https://evcoms.com',
+	},
+	{
+        name: 'Folkingtons',
+		url: 'https://www.folkingtons.com',
+	},
+	{
+        name: 'Margee',
+		url: 'https://margee.com',
+	},
+	{
+        name: 'Power Laptop',
+		url: 'https://powerlaptop.co.uk',
+	},
+    {
+        name: 'Ramona Albert',
+		url: 'https://ramonaalbert.shop',
+	},
+    {
+        name: 'TOPL',
+		url: 'https://toplcup.com',
+	},
+];
+
+export default storesList;
