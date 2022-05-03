d9382edf512968d03175b607230466d2e73386bf
lp <laur@pop-os.localdomain>
2022-05-03 11:20:07 +0300

    company vision  markup

--- src/components/company/company-vision.js
+++ src/components/company/company-vision.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Title, Content } from 'bloomer';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
 
 const CompanyVision = () => {
+
     return (
         <div>
-            CompanyVision
+            <div className="heading-block mb-6">
+					<Title className='has-text-black' tag="h2" isSize={2}>
+                        Our <span>Vision</span>
+					</Title>
+
+                    <div className='is-flex'>
+                        <p className='has-text-black'>
+                            "Our Vision...There are many variations of passages of Lorem Ipsum available, but the majority
+                            have suffered alteration in some form, by injected humour, or randomised words which
+                            don't look even slightly believable."
+                        </p>
+
+                        <div className="footer-logo">
+                            <StaticImage
+                                src='../../images/logo-vevol-media.svg'
+                                alt="avatar"
+                                layout="fullWidth"
+                            />
+                            <span className='has-text-black'>Name</span>
+                            <p className='has-text-black'>Company Owner</p>
+                        </div>
+                    </div>
+			</div>
+
+            <Link to="/">Contact</Link>
         </div>
     );
 }
--- src/pages/company-info.js
+++ src/pages/company-info.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import AboutUs from '../components/company/about-us';
 import CompanyVision from '../components/company/company-vision';
+import Layout from '../components/layout/layout';
+import SlimHero from '../components/slim-hero/slim-hero';
+import { Container } from 'bloomer';
 
 const CompanyInfo = () => {
     return (
-        <div>
-            <CompanyVision/>
-            <AboutUs/>
-        </div>
+        <Layout>
+            <SlimHero
+				heading="Our mission"
+				subheading="A wide range of talent ."
+			/>
+            
+            <div className="vm-section vm-section--white">
+                <Container>
+                    <CompanyVision/>
+                    <AboutUs/>
+                </Container>
+            </div>
+        </Layout>
     );
 }
 
