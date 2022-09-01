a5156721b9bdac8cdfbbcb731c8d21dd528ba5fa
Bogdan Dogaru <bogdan5dogaru@gmail.com>
2022-09-01 16:43:24 +0100

    Small changes to image with text component

--- src/components/general-components/image-text-simple.js
+++ src/components/general-components/image-text-simple.js
@@ -7,15 +7,17 @@ export default function ImageWithText({ image, title, textContent, alignRight })
 		<div className={`image-with-text ${alignRight && 'image-with-text--align-right'}`}>
 			<Container>
 				<div className="iwt__left">{image}</div>
-				<div className="iwt__right">
-					{title && <Title>{title}</Title>}
-					{textContent &&
-						textContent.map((item, index) => (
-							<p className="mt-3" key={index}>
-								{item}
-							</p>
-						))}
-				</div>
+				<Fade bottom cascade>
+					<div className="iwt__right">
+						{title && <Title>{title}</Title>}
+						{textContent &&
+							textContent.map((item, index) => (
+								<p className="mt-3" key={index}>
+									{item}
+								</p>
+							))}
+					</div>
+				</Fade>
 			</Container>
 		</div>
 	);
