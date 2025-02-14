f3bdd8b14972c8dba1d212215f694a527841934b
Bogdan Dogaru <bogdan@vevolmedia.com>
2025-02-14 14:46:19 +0200

    meetup updates

--- src/enums/meetup-schedule-2025.js
+++ src/enums/meetup-schedule-2025.js
@@ -1,55 +1,103 @@
 const meetupSchedule = [
 	{
-		title: '09:00 - 09:20',
-		info: `Guests arrival / Coffee time`,
+		title: '08:30 - 09:00',
+		info: 'Registration & Check-in',
 	},
 	{
-		title: '09:30 - 09:55',
-		info: `TBD`,
+		title: '09:00 - 09:10',
+		info: 'Opening Remarks',
 	},
 	{
-		title: '10:00 - 10:25',
-		info: `TBD`,
+		title: '09:10 - 09:25',
+		info: 'Andrew Cargill - CEO @ Zapiet',
 	},
 	{
-		title: '10:30 - 10:55',
-		info: `TBD`,
+		title: '09:25 - 09:30',
+		info: 'Speaker Transition',
 	},
 	{
-		title: '11:00 - 11:45',
-		info: `TBD`,
+		title: '09:30 - 10:15',
+		info: 'Catalin Ciochina - Co-Founder, CEO @ xConnector',
 	},
 	{
-		title: '11:45 - 12:15',
-		info: `TBD`,
+		title: '10:15 - 10:20',
+		info: 'Speaker Transition',
 	},
 	{
-		title: '12:20 - 12:45',
-		info: `TBD`,
+		title: '10:20 - 10:35',
+		info: 'Ruben Bristian - Co-Founder, CEO @ Krown Themes',
 	},
 	{
-		title: '12:45 - 14:00',
-		info: `TBD`,
+		title: '10:35 - 10:40',
+		info: 'Speaker Transition',
+	},
+	{
+		title: '10:40 - 10:55',
+		info: 'Available slot for keynote speaker',
+	},
+	{
+		title: '10:55 - 11:00',
+		info: 'Break Announcement',
+	},
+	{
+		title: '11:00 - 11:30',
+		info: 'Networking Break',
+	},
+	{
+		title: '11:30 - 11:35',
+		info: 'Speaker Transition',
+	},
+	{
+		title: '11:35 - 12:05',
+		info: 'Maurice Junghans from Shopify',
+	},
+	{
+		title: '12:05 - 12:10',
+		info: 'Speaker Transition',
+	},
+	{
+		title: '12:10 - 12:45',
+		info: 'Bogdan Dogaru - Co-founder & CTO @ Vevol Media',
+	},
+	{
+		title: '12:45 - 13:00',
+		info: 'Break Announcement',
+	},
+	{
+		title: '13:00 - 14:00',
+		info: 'Lunch Break',
 	},
 	{
 		title: '14:00 - 15:00',
-		info: `TBD`,
+		info: 'Panel Discussion: Dana @ Frisbo, Ruben, Andrei - Dispotrading',
 	},
 	{
-		title: '15:05 - 15:30',
-		info: `TBD`,
+		title: '15:00 - 15:35',
+		info: 'Available slot for keynote speaker',
 	},
 	{
-		title: '15:35 - 16:00',
-		info: `TBD`,
+		title: '15:35 - 15:40',
+		info: 'Speaker Transition',
 	},
 	{
-		title: '16:00 - 16:30',
-		info: `TBD`,
+		title: '15:40 - 15:55',
+		info: 'Available slot for keynote speaker',
 	},
 	{
-		title: '17:00 - Late',
-		info: `TBD`,
+		title: '15:55 - 16:00',
+		info: 'Speaker Transition',
+	},
+	{
+		title: '16:00 - 16:35',
+		info: 'Available slot for keynote speaker',
+	},
+	{
+		title: '16:35 - 16:45',
+		info: 'Giveaway Session',
+	},
+	{
+		title: '16:45 - 17:00',
+		info: 'Closing Remarks & After-party Announcement',
 	},
 ];
 
