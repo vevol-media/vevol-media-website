630042ba15fae999ece1e6eee7c7ee4bf07638b9
Bogdan Dogaru <bogdan@vevolmedia.com>
2025-07-04 16:07:34 +0300

    reviews fix

--- src/components/reviews-section/reviews-section.js
+++ src/components/reviews-section/reviews-section.js
@@ -4,7 +4,9 @@ import SingleReview from './single-review';
 import './reviews-section.scss';
 
 export default function ReviewsSection({ whiteBackground, romanianReviews }) {
-	const reviews = romanianReviews ? allReviews.filter((review) => review.isRomanian) : allReviews;
+	const reviews = romanianReviews
+		? allReviews.filter((review) => review.isRomanian)
+		: allReviews.filter((review) => !review.isRomanian);
 
 	return (
 		<ul className={`reviews-section ${whiteBackground ? 'reviews-section--white' : ''}`}>
