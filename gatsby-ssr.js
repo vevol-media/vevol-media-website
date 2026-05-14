a6e2a41887b696e8076a87dacb86b25576760b7d
Miruna Andriesei <92790594+AndrieseiMiruna@users.noreply.github.com>
2026-05-14 11:05:04 +0300

    feat: add VevolMedia script for tracking and remove duplicate vtag AI script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,7 @@ import RootElement from './src/components/root/root';
 
 export const onRenderBody = ({ setHeadComponents }) => {
 	setHeadComponents([<script key="ClutchWidgetScript" src="https://widget.clutch.co/static/js/widget.js" />]);
+	setHeadComponents([<script key="VevolMediaScript" src="https://r2.leadsy.ai/tag.js" data-pid="LqpyEn9xK88UapNc" data-version="062024" />]);
 };
 
 export const wrapRootElement = ({ element }) => {
